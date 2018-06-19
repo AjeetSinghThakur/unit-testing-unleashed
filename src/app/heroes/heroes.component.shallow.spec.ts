1de4d305c0141724e60436a34ade7564ebe58118
Ajeet Thakur <ajeetthakur@Ajeets-MacBook-Pro.local>
2018-06-19 11:27:29 +0300

    Typescript error resolver.

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -7,50 +7,49 @@ import { NO_ERRORS_SCHEMA, Component, Input } from '@angular/core';
 import { of } from 'rxjs/observable/of';
 import { Hero } from '../hero';
 
-describe('Hero Component(shallow test)',() => {
-    let fixture:ComponentFixture<HeroesComponent>;
+describe('Hero Component(shallow test)', () => {
+    let fixture: ComponentFixture<HeroesComponent>;
     let mockHeroService;
     let HEROES;
-    
     @Component({
         selector: 'app-hero',
         template: '<div></div>',
     })
     class FakeHeroComponent {
         @Input() hero: Hero;
-        //@Output() delete = new EventEmitter();
+        // @Output() delete = new EventEmitter();
     }
 
     beforeEach(() => {
         HEROES = [
-            {id:1, name: 'SpiderDude', strength: 8},
-            {id:2, name: 'Wonderful Woman', strength: 24},
-            {id:3, name: 'SuperDude', strength: 55}
-          ]
-        mockHeroService = 
-        jasmine.createSpyObj(['getHeroes','addHero','deleteHero ']);
+            {id: 1, name: 'SpiderDude', strength: 8},
+            {id: 2, name: 'Wonderful Woman', strength: 24},
+            {id: 3, name: 'SuperDude', strength: 55}
+          ];
+        mockHeroService =
+        jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero ']);
 
         TestBed.configureTestingModule({
-            declarations:[
+            declarations: [
                 HeroesComponent,
                 FakeHeroComponent
             ],
-            providers:[
-                { provide: HeroService,useValue: mockHeroService }
+            providers: [
+                { provide: HeroService, useValue: mockHeroService }
             ],
-            //schemas:[NO_ERRORS_SCHEMA]
+            // schemas:[NO_ERRORS_SCHEMA]
         });
         fixture = TestBed.createComponent(HeroesComponent);
     });
-    it('should set heros correctly from the service',() =>{
-        mockHeroService.getHeroes.and.returnValue(of(HEROES))
+    it('should set heros correctly from the service', () => {
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
         fixture.detectChanges();
         expect(fixture.componentInstance.heroes.length).toBe(3);
     });
-    it('should create one li for each hero',() =>{
-        mockHeroService.getHeroes.and.returnValue(of(HEROES))
+    it('should create one li for each hero', () => {
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
         fixture.detectChanges();
-        
+
         expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
     });
 });
--- src/app/heroes/heros.component.spec.ts
+++ src/app/heroes/heros.component.spec.ts
@@ -1,32 +1,32 @@
-import { HeroesComponent } from "./heroes.component";
-import { of } from "rxjs/observable/of";
+import { HeroesComponent } from './heroes.component';
+import { of } from 'rxjs/observable/of';
 
-describe('HeroComponent',() => {
+describe('HeroComponent', () => {
     let component: HeroesComponent;
     let HEROES;
     let mockHeroService;
 
-    beforeEach(() =>{
+    beforeEach(() => {
         HEROES = [
-            {id:1, name: 'SpiderDude', strength: 8},
-            {id:2, name: 'Wonderful Woman', strength: 24},
-            {id:3, name: 'SuperDude', strength: 55}
-          ]
+            {id: 1, name: 'SpiderDude', strength: 8},
+            {id: 2, name: 'Wonderful Woman', strength: 24},
+            {id: 3, name: 'SuperDude', strength: 55}
+          ];
     });
-    mockHeroService = 
-    jasmine.createSpyObj(['getHeroes','addHero','deleteHero']);
+    mockHeroService =
+    jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
 
     component = new HeroesComponent(mockHeroService);
 
-    describe('delete',() =>{
-        it('should remove the indicated hero from heros list',() => {
+    describe('delete', () => {
+        it('should remove the indicated hero from heros list', () => {
             mockHeroService.deleteHero.and.returnValue(of(true));
             component.heroes = HEROES;
 
             component.delete(HEROES[2]);
             expect(component.heroes.length).toBe(2);
         });
-        it('should call deleteHero',() => {
+        it('should call deleteHero', () => {
             mockHeroService.deleteHero.and.returnValue(of(true));
             component.heroes = HEROES;
 
@@ -34,5 +34,5 @@ describe('HeroComponent',() => {
             expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
         });
     });
-    
+
 });
