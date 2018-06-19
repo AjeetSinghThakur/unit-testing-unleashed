196860d908988f073f49d18f018f727252852a49
Ajeet Thakur <ajeetthakur@Ajeets-MacBook-Pro.local>
2018-06-19 11:31:00 +0300

    Modified code to fix space issues.

--- src/app/hero/hero.component.spec.ts
+++ src/app/hero/hero.component.spec.ts
@@ -1,5 +1,5 @@
 import { HeroComponent } from './hero.component';
-import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
@@ -8,22 +8,21 @@ describe('HeroComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations:[HeroComponent],
-            schemas:[NO_ERRORS_SCHEMA]
+            declarations: [HeroComponent],
+            schemas: [NO_ERRORS_SCHEMA]
         });
         fixture = TestBed.createComponent(HeroComponent);
     });
-    
-    it('should have the correct hero',() => {
-        fixture.componentInstance.hero = { id: 1, name: 'SuperDude',strength:3 };
+    it('should have the correct hero', () => {
+        fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
         expect(fixture.componentInstance.hero.name).toEqual('SuperDude');
     });
-    it('should render the hero name inside anchor tag',() => {
-        fixture.componentInstance.hero = { id: 1, name: 'SuperDude',strength:3 };
+    it('should render the hero name inside anchor tag', () => {
+        fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
         fixture.detectChanges();
-        let deA = fixture.debugElement.query(By.css('a'));
+        const deA = fixture.debugElement.query(By.css('a'));
 
         expect(deA.nativeElement.textContent).toContain('SuperDude');
-        //expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
+        // expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
     });
 });
--- src/app/messages/message.service.spec.ts
+++ src/app/messages/message.service.spec.ts
@@ -1,20 +1,20 @@
 import { MessageService } from './../message.service';
 
-describe('MessageService',() => {
+describe('MessageService', () => {
     let service: MessageService;
 
     beforeEach(() => {
         service = new MessageService();
     });
 
-    it('should have no messages to start',() => {
+    it('should have no messages to start', () => {
         expect(service.messages.length).toEqual(0);
     });
-    it('should add a message when add is called',() => {
+    it('should add a message when add is called', () => {
         service.add('message1');
         expect(service.messages.length).toEqual(1);
     });
-    it('should remove a message when clear is called',() => {
+    it('should remove a message when clear is called', () => {
         service.add('message1');
         service.clear();
         expect(service.messages.length).toEqual(0);
